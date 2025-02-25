0aedc597295ba57dfbcfbe6711e9f0a66352efba
alsdk9843 <44119261+alsdk9843@users.noreply.github.com>
2025-02-25 11:37:38 +0530

    Add files via upload

---
+++ Inheritance_and_Polymorphism.cs
@@ -0,0 +1,63 @@
+/*
+public interface IAnimal
+{
+    void Eat();
+}
+
+public class Inheritance_and_Polymorphism
+{
+    public static void Main(string[] args)
+    {
+        Animal mammal = new Animal();
+        Animal mydog = new Dog();
+        Animal mycat = new Cat();
+        
+        mammal.MakeSound();
+        mydog.MakeSound();
+        mycat.MakeSound();
+        
+        mammal.Eat();
+        mydog.Eat();
+        mycat.Eat();
+    }
+}
+
+public class Animal : IAnimal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound.");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating food.");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog Barks.");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Dog is eating food.");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat Meows.");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Cat is eating food.");
+    }
+}
+*/
